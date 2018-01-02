ca92acbffcb39b34173fe157c348f34a3f701179
engine063 <943788963@qq.com>
2018-01-02 14:35:36 +0700

    init project

---
+++ game/src/Main.ts
@@ -0,0 +1,49 @@
+
+class Main extends eui.UILayer {
+
+    protected createChildren(): void {
+        super.createChildren();
+        // console.info('调试提示：改变端口可在链接中添加port参数，如设置端口30100可添加“?port=30100”，默认31100')
+        egret.registerImplementation("eui.IAssetAdapter",new AssetAdapter());
+        egret.registerImplementation("eui.IThemeAdapter",new ThemeAdapter());
+        RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.loadConfig("resource/default.res.json", "resource/");
+    }
+    private onConfigComplete(event:RES.ResourceEvent):void {
+        RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        com_main.ResLoad.getInst().loadGroupWithCall(['start'], this.onResourceLoadComplete, this, 'com_main.LoadComponent');
+        let theme = new eui.Theme("resource/default.thm.json", this.stage);
+        theme.addEventListener(eui.UIEvent.COMPLETE, this.onThemeLoadComplete, this);
+        this.stage.addEventListener(egret.Event.RESIZE,this.stageResize,this);
+    }
+    private isThemeLoadEnd: boolean = false;
+    private onThemeLoadComplete(): void {
+        this.isThemeLoadEnd = true;
+        this.createScene();
+    }
+    private isResourceLoadEnd: boolean = false;
+    private onResourceLoadComplete():void {
+        this.isResourceLoadEnd = true;
+        this.createScene();
+        AGame.HttpClient.serverUrl = RES.getRes('global').urlHttp;//"http://127.0.0.1:18080";
+        AGame.HttpClient.get((data)=>{
+            console.log('/', data);
+            RES.getRes('global').urlSocket = data.urlSocket;
+            AGame.CSocket.getInstance().connect(data.urlSocket);
+        }, this, '');
+    }
+
+    private createScene(){
+        if(this.isThemeLoadEnd && this.isResourceLoadEnd) {
+            com_main.Bootstrap.startup(this);
+            AGame.R.notifyObserver(StartNav.ADD_START);
+        }
+    }
+
+    // 舞台尺寸发生变化的响应事件 
+    private stageResize(evt:egret.Event) {
+        com_main.SceneManager.stageSizeChanged();
+        com_main.UpManager.stageSizeChanged();
+    }
+    
+}
---
+++ game/src/com_main/data/UserInfo.ts
@@ -0,0 +1,35 @@
+
+
+class UserInfo {
+
+    public uId :string;
+    public nick:string;
+    public photo:string;
+    public nPos:number;
+    public bOnline :boolean = true;
+    public skin :number;
+
+    public arrFly :Array<FlyInfo>;// 棋子信息 
+
+    constructor(uId:string, nick:string, photo:string, nPos:number, skin:number) {
+        this.uId = uId;
+        this.nick = nick;
+        this.photo = photo;
+        this.nPos = nPos;
+        this.skin = skin;
+
+        this.arrFly = [];
+        for(var i:number=0,fly:FlyInfo; i<3; i++){
+            fly = new FlyInfo();
+            this.arrFly.push(fly);
+        }
+
+    }
+
+    private onOffLine() {
+
+    }
+
+
+
+}
